b95ebf95dcc45b0c9ee08b8b10b6d01ca98a6402
s24340 <s24340@pjwstk.edu.pl>
2024-03-16 09:51:32 +0100

    Funkcja obliczajaca srednia

--- Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fee8e5e2bd3b9e8e14422029f5b0d2f8c048421")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65447ab72c9ef4179708f9c1a13a6881012b93e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
